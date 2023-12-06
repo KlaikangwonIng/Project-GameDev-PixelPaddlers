090cd113cce6ce756de48e8f0cba97a66eecbaee
tontonrodpenrai <142320807+tontonrodpenrai@users.noreply.github.com>
2023-12-06 15:13:58 +0700

    Update GameScene.js

--- src/scene/GameScene.js
+++ src/scene/GameScene.js
@@ -1,30 +1,53 @@
 import Phaser from "phaser";
 
 
-class GameScence extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
   constructor() {
     super({
       key: "GameScene",
     });
     this.bg
+    this.platformtop
+    this.stonetop
+    this.platformbottom
+    this.stonebottom
     this.player
   }
 
   preload() {
     this.load.image(
       "bg-underwater",
-      "underwater-diving-files/underwater-diving-files/PNG/environment/background.png"
+      "assets/PNG/environment/background.png"
     );
     
     this.load.spritesheet(
       "player",
-      "underwater-diving-files/underwater-diving-files/PNG/player/player-swiming.png",
+      "assets/PNG/player/player-swiming.png",
       {
         frameWidth: 81,
         frameHeight: 80,
       }
+    );
+
+    this.load.image(
+      "bg-platformtop",
+      "assets/PNG/elements/layerplatform-top.png"
+    );
+
+    this.load.image(
+      "bg-stonetop",
+      "assets/PNG/elements/stone-top.png"
+    );
+
+    this.load.image(
+      "bg-platformbottom",
+      "assets/PNG/elements/layerplatform-bottom.png"
     )
 
+    this.load.image(
+      "bg-stonebottom",
+      "assets/PNG/elements/stone-bottom.png"
+    );
   }
 
   create() {
@@ -33,7 +56,8 @@ class GameScence extends Phaser.Scene {
       .setOrigin(0, 0)
       .setScale(3);
       console.log(this.bg);
-    this.player = this.physics.add.sprite(700, 350, 'player')
+    this.player = this.physics.add.sprite(200, 350, 'player')
+      .setScale(1.5);
 
     this.anims.create({
       key: "player",
@@ -41,17 +65,32 @@ class GameScence extends Phaser.Scene {
         start: 0,
         end: 6,
       }),
-      frameRate: 6,
+      frameRate: 7,
       repeat: -1,
     })
 
+    this.platformtop = this.add.tileSprite(0, 0, 1280, 720, "bg-platformtop")
+    .setOrigin(0, 0);
 
+    this.stonetop = this.add.tileSprite(0, 0, 1280, 720, "bg-stonetop")
+    .setOrigin(0, 0);
+
+    this.platformbottom = this.add.tileSprite(0, 30, 1280, 720, "bg-platformbottom")
+    .setOrigin(0, 0);
+
+    this.stonebottom = this.add.tileSprite(0, 50, 1280, 720, "bg-stonebottom")
+    .setOrigin(0, 0)
+    .setScale(1);
   }
 
   update() {
     this.bg.tilePositionX += .7;
-    this.player.anims.play('player', true)
+    this.platformtop.tilePositionX += 0.7;
+    this.stonetop.tilePositionX += 0.7;
+    this.platformbottom.tilePositionX += 0.7;
+    this.stonebottom.tilePositionX += 0.7;
+    this.player.anims.play(`player`, true)
   }
 }
 
-export default GameScence;
+export default GameScene;
